c011e6fa1e449caf09416df6a9719022b0ba45b8
samzzii <nasstromsamuel@icloud.com>
2023-11-28 00:30:34 +0100

    Added base template, expanded views and did some logic

--- movies/views.py
+++ movies/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'movies/index.html', {})
+    context = {
+        'movies': [
+            'Lord of The Rings',
+            'Gladiator',
+            'Harry Potter and The Half Blood Prince',
+            'Deadpool',
+            'The Silence of The Lambs',
+            'Avatar: Way of Water',
+            'The Internship',
+        ],
+        'tvseries': [
+            'Friends',
+            'TVD',
+            'The walking dead',
+            'Charmed',
+        ]
+    }
+    return render(request, 'movies/index.html', context)
 
 def about(request):
     return render(request, 'movies/about.html', {})
