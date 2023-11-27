2308813937016e90bf53c99ef22d10b3aa04a948
samzzii <nasstromsamuel@icloud.com>
2023-11-27 16:18:54 +0100

    added index and about view

--- movies/views.py
+++ movies/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("My Fav Movies")
+    return render(request, 'movies/index.html', {})
+
+def about(request):
+    return render(request, 'movies/about.html', {})
+
+#app/templates/app/index.html
+#movies/templates/movies/index.html
